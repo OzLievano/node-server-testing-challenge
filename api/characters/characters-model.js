a14595d747b69e854baf933c705970925aa4ebd2
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2021-02-18 21:14:06 -0500

    tested endpoints focharacter routes

--- api/characters/characters-model.js
+++ api/characters/characters-model.js
@@ -2,6 +2,7 @@ const db = require('../../data/dbConfig');
 
 module.exports = {
     get,
+    findById,
     create,
     remove
 }
@@ -10,10 +11,15 @@ async function get(){
     return await db('characters');
 }
 
+async function findById(id){
+    return await db('characters').where({ id }).first();
+}
+
 async function create(character){
-    return character;
+    const [id] = await db('characters').insert(character,"id")
+    return await findById(id);
 }
 
 async function remove(id){
-    return id;
+    return await db('characters').where({id}).del();
 }
--- api/characters/characters-router.js
+++ api/characters/characters-router.js
@@ -11,4 +11,37 @@ router.get('/', function getAllCharacters(req,res){
     })
 })
 
+router.get('/:id', function getAllCharacters(req,res){
+    const {id} = req.params;
+    Characters.findById(id)
+    .then((character)=>{
+        res.status(200).json(character);
+    })
+    .catch((err)=>{
+        res.status(500).json({error:err.message})
+    })
+})
+
+router.delete('/:id',function removeCharacter(req,res){
+    const {id} = req.params;
+
+    Characters.remove(id)
+    .then((character)=>{
+        res.status(200).json({message:`Character number ${id} deleted`})
+    })
+    .catch((err)=>{
+        res.status(500).json({error:err.message})
+    })
+})
+
+router.post('/', function createCharacter(req,res){
+    const character = req.body;
+    Characters.create(character)
+    .then((character)=>{
+        res.status(201).json(character);
+    })
+    .catch((err)=>{
+        res.status(500).json({error:err.message})
+    })
+})
 module.exports = router;
